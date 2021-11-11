e1f10176411a1ee16f23f1d17c970cc3d70bdbdd
Robert <64583205+Robert565@users.noreply.github.com>
2021-11-11 20:51:11 +0900

    added temporary plug

--- Application/webhook.py
+++ Application/webhook.py
@@ -2,11 +2,15 @@ from flask import Flask, request
 
 from .methods import Signals
 from .utils.db import ControlDatabase
-from .utils.API import API
+from .utils.API import API, ErrorVK
 
 app = Flask(__name__)
 DB = ControlDatabase()
-vk = API(token=DB.token)
+try:
+    vk = API(token=DB.token)
+except ErrorVK:
+    DB.update_config()
+    vk = API(token=DB.token)
 
 
 def Callback():
--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 from Application.webhook import main, app
 
+
 if __name__ == '__main__':
     main()
     app.run(port=80, host='0.0.0.0')
