070a0967bcb066644777b0d296ed09f890eba354
Robert <64583205+Robert565@users.noreply.github.com>
2022-04-25 06:21:11 +0900

    added auto installation Flask to start through PAW

--- start_on_PAW.py
+++ start_on_PAW.py
@@ -1,3 +1,9 @@
 from Application.webhook import main, app
+import os
+import sys
+
+for module in ['flask']:
+    if module not in sys.modules:
+        os.system('python3 -m pip install {} --user'.format(module))
 
 main()
